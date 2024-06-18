5bcff048aa0582dc9df8faf08755192cb12d12f4
Mark Harris <markymark1990@hotmail.co.ukgit config --global user.name markymark1990lsgit config --global user.email markymark1990@hotmail.co.uk>
2024-06-18 16:28:45 +0100

    Added vote function to articles, updated to suit comments from last task submission

--- src/components/ArticleInfo.jsx
+++ src/components/ArticleInfo.jsx
@@ -1,4 +1,4 @@
-import { getArticleById } from "../utils/api.js";
+import { getArticleById, voteOnArticle } from "../utils/api.js";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import CommentsByArticle from "./CommentsByArticle.jsx";
@@ -6,8 +6,9 @@ import CommentsByArticle from "./CommentsByArticle.jsx";
 const ArticleInfo = () => {
 
     const { articleId } = useParams()
-    const [article, setArticle] = useState ({})
-    const [isLoading, setIsLoading] = useState (true)
+    const [article, setArticle] = useState({})
+    const [isLoading, setIsLoading] = useState(true)
+    const [voted, setVoted] = useState(false)
 
     useEffect(() => {
         getArticleById(articleId).then((response) => {
@@ -16,20 +17,40 @@ const ArticleInfo = () => {
         })
     }, [articleId])
 
+
+    const vote = (voteType) => {
+        if (voted) {
+            return setArticle((prevArticle) => ({
+                ...prevArticle,
+                votes: voteType === 'up' ? prevArticle.votes + 1 : prevArticle.votes - 1,
+            }));
+        }
+        setVoted(true)
+
+        voteOnArticle(articleId, voteType)
+            .then((response) => {
+                setArticle(response.data.article);
+                console.log("Vote successful:", response.data);
+            })
+    }
+
     return (
         <div>
             {isLoading ? (
                 <p>Loading...</p>
             ) : (
-        <div className="article-list"> 
-            <h2>{article.title}</h2>
-            <img src={article.article_img_url} alt={article.title} className="article-image" />
-            <p><strong>Author:</strong> {article.author}</p>
-            <p><strong>Topic:</strong> {article.topic}</p>
-            <p><strong>Created at:</strong> {new Date(article.created_at).toLocaleDateString()}</p>
-            <p>{article.body}</p>
-            <CommentsByArticle />
-        </div>
+                <div className="article-list">
+                    <h2>{article.title}</h2>
+                    <img src={article.article_img_url} alt={article.title} className="article-image" />
+                    <p><strong>Author:</strong> {article.author}</p>
+                    <p><strong>Topic:</strong> {article.topic}</p>
+                    <p><strong>Created at:</strong> {new Date(article.created_at).toLocaleDateString()}</p>
+                    <p>{article.body}</p>
+                    <p><strong>Votes:</strong>{article.votes}</p>
+                    <button onClick={() => vote('up')}>Vote Up</button>
+                    <button onClick={() => vote('down')}>Vote Down</button>
+                    <CommentsByArticle />
+                </div>
             )}
         </div>
     )
